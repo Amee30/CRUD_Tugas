1435c1585250cd0c421d8e864f14a977b3eb0224
Ame <darmaputra2004@gmail.com>
2023-05-22 00:14:55 +0800

    Update DB

--- database/dosen.sql
+++ database/dosen.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 18 Bulan Mei 2023 pada 15.11
+-- Waktu pembuatan: 21 Bulan Mei 2023 pada 18.14
 -- Versi server: 10.4.27-MariaDB
 -- Versi PHP: 8.2.0
 
@@ -29,10 +29,18 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `dosenmember` (
   `id` int(11) NOT NULL,
-  `nama` varchar(15) NOT NULL,
+  `nama` varchar(64) NOT NULL,
   `nip` int(15) NOT NULL,
-  `alamat` varchar(20) NOT NULL
+  `alamat` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `dosenmember`
+--
+
+INSERT INTO `dosenmember` (`id`, `nama`, `nip`, `alamat`) VALUES
+(1, 'Komang Tretan Muslim', 328427112, 'Perumahan Jalak No 2'),
+(2, 'Muhammad Ricko Recing Mandalika', 412232273, 'Jalan Raya Mandalika Rawa Gede 69');
 
 -- --------------------------------------------------------
 
@@ -42,9 +50,9 @@ CREATE TABLE `dosenmember` (
 
 CREATE TABLE `mahasiswamember` (
   `id` int(11) NOT NULL,
-  `nama` varchar(25) NOT NULL,
+  `nama` varchar(50) NOT NULL,
   `nim` varchar(10) NOT NULL,
-  `alamat` varchar(20) NOT NULL
+  `alamat` varchar(125) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -52,7 +60,8 @@ CREATE TABLE `mahasiswamember` (
 --
 
 INSERT INTO `mahasiswamember` (`id`, `nama`, `nim`, `alamat`) VALUES
-(1, 'Muhammad Joko K', '2215354099', 'Perumahan Merak No 1');
+(1, 'Muhammad Joko Kopling', '2215354090', 'Perumahan Merak No 2'),
+(2, 'Alianto Cahyadi', '2215356048', 'Jalan Tulung Agung N');
 
 -- --------------------------------------------------------
 
@@ -63,16 +72,17 @@ INSERT INTO `mahasiswamember` (`id`, `nama`, `nim`, `alamat`) VALUES
 CREATE TABLE `usermember` (
   `id` int(11) NOT NULL,
   `usertable` varchar(10) NOT NULL,
-  `passtable` int(10) NOT NULL
+  `passtable` int(10) NOT NULL,
+  `usertype` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `usermember`
 --
 
-INSERT INTO `usermember` (`id`, `usertable`, `passtable`) VALUES
-(1, 'dosen', 2004),
-(2, 'mahasiswa', 1234);
+INSERT INTO `usermember` (`id`, `usertable`, `passtable`, `usertype`) VALUES
+(1, 'dosen', 2004, 'dosen'),
+(2, '2215354007', 1234, 'mahasiswa');
 
 --
 -- Indexes for dumped tables
@@ -104,13 +114,13 @@ ALTER TABLE `usermember`
 -- AUTO_INCREMENT untuk tabel `dosenmember`
 --
 ALTER TABLE `dosenmember`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `mahasiswamember`
 --
 ALTER TABLE `mahasiswamember`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `usermember`
